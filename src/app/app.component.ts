8ee3f70a7508341d541a9f3f8d0c9e8ef3e6d52c
Mohamed AlMokadem <Mohamed.AlMokadem@vodafone.com>
2020-09-06 15:05:27 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
+  
+  constructor(private translateService: TranslateService) {}
+   changeLangage(lang: string) {
+   this.translateService.setDefaultLang(lang);
+   this.translateService.use(lang);
+}
+
   title = 'angular-localization';
+
 }
