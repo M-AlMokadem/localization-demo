b6dc36d6458981ff2a6db55287b694ca5f12a0a2
Mohamed AlMokadem <Mohamed.AlMokadem@vodafone.com>
2020-09-10 01:30:20 +0200

    adding dynamic css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { DOCUMENT } from "@angular/common";
 
 
 @Component({
@@ -10,13 +11,49 @@ import { TranslateService } from '@ngx-translate/core';
 
 
 export class AppComponent {
-  
-  constructor(private translateService: TranslateService) {}
-   changeLangage(lang: string) {
-   this.translateService.setDefaultLang(lang);
-   this.translateService.use(lang);
-}
 
   title = 'angular-localization';
+
+  constructor(private translateService: TranslateService,
+    @Inject(DOCUMENT) private document: Document) 
+    {}
+
+    changeLangage(lang: string) {
+      console.log(this.document.getElementsByTagName(
+        "html"
+      ))
+      let htmlTag = this.document.getElementsByTagName(
+        "html"
+      )[0] as HTMLHtmlElement;
+      htmlTag.dir = lang === "ar" ? "rtl" : "ltr";
+      this.translateService.setDefaultLang(lang);
+      this.translateService.use(lang);
+      this.changeCssFile(lang);
+    }
+
+    changeCssFile(lang: string) {
+      let headTag = this.document.getElementsByTagName(
+        "head"
+      )[0] as HTMLHeadElement;
+      let existingLink = this.document.getElementById(
+        "langCss"
+      ) as HTMLLinkElement;
+  
+      let bundleName = lang === "ar" ? "arabicStyle" : "englishStyle";
+  
+      if (existingLink) {
+        existingLink.href = bundleName;
+        console.log('existingLink ---> ',existingLink.href);
+      } else {
+        let newLink = this.document.createElement("link");
+        newLink.rel = "stylesheet";
+        newLink.type = "text/css";
+        newLink.id = "langCss";
+        newLink.href = bundleName;
+        headTag.appendChild(newLink);
+      }
+    }
+
+  
 
 }
